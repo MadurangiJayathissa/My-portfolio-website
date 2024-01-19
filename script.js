77d00df1bc4f585095bdbceb74a33512c6be2fe2
Madurangi Jayathissa <92798397+MadurangiJayathissa@users.noreply.github.com>
2024-01-19 12:01:21 +0530

    My Web ✅

---
+++ script.js
@@ -0,0 +1,38 @@
+// Header toogle
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+
+// Typing Effect
+
+let typed = new Typed('.auto-input',{
+    strings: ['Front-End Developer!', 'Freelancer!', 'UI/UX Designer!', 'Backend Developer!'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+}) 
+
+//Get All Links
+let navLinks = document.querySelectorAll('nav ul li a')
+//Get All Section
+let sections = document.querySelectorAll('section')
+
+window.addEventListener('scroll', function (){
+    const scrollPos = window.scrollY + 20
+    sections.forEach(section => {
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if(section.getAttribute('id') === link.getAttribute('herf').substring(1)) {
+                    link.classList.add('active')
+                }
+            });
+        }
+    });
+});
